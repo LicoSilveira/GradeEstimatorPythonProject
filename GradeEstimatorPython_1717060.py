9a7480a5293370151cca563cb4277f977de2ad34
Licurgo Teixeira <1717060@park.edu>
2025-01-19 06:35:16 -0600

    Grade Estimator Inicial Project

---
+++ GradeEstimatorPython_1717060.py
@@ -0,0 +1,13 @@
+# Ask for the user's first name
+first_name = input("Licurgo")
+
+# Ask for the user's last name
+last_name = input("Teixeira")
+
+# Print the full name
+print(f"{first_name}{last_name}")
+
+# Explanatory comments
+# 1. Asking the user to enter their first name and storing it in the variable first_name.
+# 2. Asking the user to enter their last name and storing it in the variable last_name.
+# 3. Printing the full name using an f-string to format the output.
